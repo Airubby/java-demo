699427e1d71ddf663d1371c044562f78160a8869
Airubby <ai_rubby@qq.com>
2019-02-15 22:24:08 +0800

    菜单第一次从数据库中取，后面从内存中取

--- store_v5/src/com/store/web/servlet/CategoryServlet.java
+++ store_v5/src/com/store/web/servlet/CategoryServlet.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import com.store.domain.Category;
 import com.store.service.CategoryService;
 import com.store.service.serviceImp.CategoryServiceImp;
+import com.store.utils.JedisUtils;
 import com.store.web.base.BaseServlet;
 
 import net.sf.json.JSONArray;
+import redis.clients.jedis.Jedis;
 
 /**
  * Servlet implementation class CategoryServlet
@@ -19,10 +21,15 @@ import net.sf.json.JSONArray;
 @WebServlet("/CategoryServlet")
 public class CategoryServlet extends BaseServlet {
 	public String findAllCats(HttpServletRequest req, HttpServletResponse resp) throws Exception{
-		//调用业务层获取全部分类；将全部分类转为json格式；并响应到客户端
-		CategoryService categoryService=new CategoryServiceImp();
-		List<Category> list=categoryService.getAllCats();
-		String jsonStr=JSONArray.fromObject(list).toString();
+		Jedis jedis=JedisUtils.getJedis();
+		String jsonStr=jedis.get("allCats");
+		//如果内存中没有数据就获取后返回；有数据了就直接返回
+		if(null==jsonStr||"".equals(jsonStr)) {
+			//调用业务层获取全部分类；将全部分类转为json格式；并响应到客户端
+			CategoryService categoryService=new CategoryServiceImp();
+			List<Category> list=categoryService.getAllCats();
+			jsonStr=JSONArray.fromObject(list).toString();
+		}
 		resp.setContentType("application/json;charset=utf-8");
 		resp.getWriter().print(jsonStr);
 		return null;
