6eec18a14eb6cc05b6b08c9812ac75f212d2a307
Airubby <ai_rubby@qq.com>
2019-02-15 21:50:29 +0800

    header.jsp中获取导航分类信息

--- store_v5/src/com/store/web/servlet/CategoryServlet.java
+++ store_v5/src/com/store/web/servlet/CategoryServlet.java
@@ -1,18 +1,30 @@
 package com.store.web.servlet;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.store.domain.Category;
+import com.store.service.CategoryService;
+import com.store.service.serviceImp.CategoryServiceImp;
 import com.store.web.base.BaseServlet;
+
+import net.sf.json.JSONArray;
 
 /**
  * Servlet implementation class CategoryServlet
  */
 @WebServlet("/CategoryServlet")
 public class CategoryServlet extends BaseServlet {
-	
+	public String findAllCats(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+		//调用业务层获取全部分类；将全部分类转为json格式；并响应到客户端
+		CategoryService categoryService=new CategoryServiceImp();
+		List<Category> list=categoryService.getAllCats();
+		String jsonStr=JSONArray.fromObject(list).toString();
+		resp.setContentType("application/json;charset=utf-8");
+		resp.getWriter().print(jsonStr);
+		return null;
+	}
 }
